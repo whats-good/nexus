336cfe0d88a15a84fc5d33dda9d138fa1a5a8c86
Kerem Kazan <kerem.kazan@gmail.com>
2023-11-13 09:41:54 -0500

    import uses esm modules now

--- packages/nexus/tsup.config.ts
+++ packages/nexus/tsup.config.ts
@@ -4,9 +4,9 @@ import { defineConfig } from "tsup";
 export default defineConfig((options: Options) => ({
   treeshake: true,
   entry: ["./src/fetch/index.ts", "./src/node/index.ts", "./src/lib/index.ts"],
-  format: ["cjs"],
+  format: ["cjs", "esm"],
   dts: true,
-  minify: true,
+  minify: false,
   clean: true,
   sourcemap: true,
   splitting: true,
