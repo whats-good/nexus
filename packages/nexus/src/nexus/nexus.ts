8ba7eb6b2169bd97a892147bdad714a8a6e981c7
Kerem Kazan <kerem.kazan@gmail.com>
2023-12-01 10:37:37 -0500

    singleton registry --> default registry

--- packages/nexus/src/nexus/nexus.ts
+++ packages/nexus/src/nexus/nexus.ts
@@ -5,6 +5,7 @@ import {
 import { RequestHandler } from "../request-handler/request-handler";
 import { Config } from "../config";
 import type { ConfigConstructorParams } from "../config";
+import { createDefaultRegistry } from "../registry";
 
 type EmptyServerContext = Record<string, never>;
 
@@ -19,6 +20,7 @@ export class NexusServer<TServerContext = EmptyServerContext>
   implements ServerAdapterBaseObject<TServerContext>
 {
   private readonly requestHandler = new RequestHandler();
+  private readonly defaultRegistry = createDefaultRegistry();
 
   private constructor(
     private readonly options: ServerContextConfigMap<TServerContext>
@@ -34,6 +36,11 @@ export class NexusServer<TServerContext = EmptyServerContext>
         typeof value === "function" ? value(serverContext, request) : value,
       ])
     );
+
+    if (!configParams.registry) {
+      configParams.registry = this.defaultRegistry;
+    }
+
     const config = new Config(configParams as ConfigConstructorParams);
 
     return this.requestHandler.handle(config, request);
--- packages/nexus/src/registry/index.ts
+++ packages/nexus/src/registry/index.ts
@@ -1,2 +1,2 @@
 export * from "./registry";
-export * from "./global-singleton-registry";
+export * from "./default-registry";
