398d81488285910095b07b7383ad4c6679210939
Kerem Kazan <kerem.kazan@gmail.com>
2023-12-07 10:19:19 -0500

    cf worker example more providers and chains

--- examples/cloudflare-worker/src/index.ts
+++ examples/cloudflare-worker/src/index.ts
@@ -11,15 +11,24 @@ type Env = Record<string, string>;
 const nexus = Nexus.create<Env>({
   // TODO: add a `createUserContext` function that generates the ctx object from the server context
   providers: (ctx) => [
-    "base",
     {
       name: "alchemy",
-      key: ctx.NEXUS_PROVIDER_ALCHEMY_KEY,
+      key: ctx.ALCHEMY_KEY,
+    },
+    {
+      name: "infura",
+      key: ctx.INFURA_KEY,
+    },
+    {
+      name: "ankr",
+      key: ctx.ANKR_KEY,
     },
   ],
   // TODO: maybe this should actually be just an env var?
   globalAccessKey: (ctx) => ctx.NEXUS_GLOBAL_ACCESS_KEY,
-  chains: [84531],
+  chains: [1, 11155111],
 });
 
-export default { fetch: nexus.fetch };
+export default {
+  fetch: nexus.fetch,
+};
