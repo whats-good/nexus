272f0f09b3773e5e2c7513eb1c18a38d0bd18bc3
Kerem Kazan <kerem.kazan@gmail.com>
2024-01-05 10:08:49 -0500

    made readEnabled optional, and introduced another optional writeEnabled field

--- packages/nexus/src/cache/rpc-request-cache.ts
+++ packages/nexus/src/cache/rpc-request-cache.ts
@@ -47,7 +47,7 @@ export class RpcRequestCache {
 
     const { cacheConfig } = methodDescriptor;
 
-    if (!cacheConfig?.enabled) {
+    if (!cacheConfig?.readEnabled) {
       this.logger.info(`Cache: Cache is disabled for method ${request.method}`);
 
       return;
@@ -135,7 +135,7 @@ export class RpcRequestCache {
 
     const { cacheConfig } = methodDescriptor;
 
-    if (!cacheConfig?.enabled) {
+    if (!cacheConfig?.readEnabled) {
       this.logger.info(`Cache: Cache is disabled for method ${request.method}`);
 
       return undefined;
