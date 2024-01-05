749469bd4d8e2d6015490bee9d44934ec1777f5d
Kerem Kazan <kerem.kazan@gmail.com>
2024-01-05 10:42:07 -0500

    brought the tuple + registry inline with the refactored method descriptor class

--- packages/nexus/src/method-descriptor/method-descriptor-registry.ts
+++ packages/nexus/src/method-descriptor/method-descriptor-registry.ts
@@ -1,4 +1,3 @@
-import { z } from "zod";
 import type {
   MethodDescriptor,
   AnyMethodDescriptor,
@@ -9,7 +8,7 @@ type MethodDescriptorTuple = readonly [
   ...AnyMethodDescriptor[],
 ];
 
-type MethodNameOf<D extends AnyMethodDescriptor> = D["methodName"];
+type MethodNameOf<D extends AnyMethodDescriptor> = D["method"];
 
 type MethodNamesOf<T extends MethodDescriptorTuple> = MethodNameOf<T[number]>;
 
@@ -21,48 +20,24 @@ type MethodDescriptorMapOf<T extends MethodDescriptorTuple> = {
     : never;
 };
 
-type RpcRequestSchemasListOf<T extends MethodDescriptorTuple> = {
-  [K in keyof T]: T[K]["requestSchema"];
-};
-type RpcRequestSchemaOf<T extends MethodDescriptorTuple> = z.ZodUnion<
-  RpcRequestSchemasListOf<T>
->;
-
 type SomeDescriptorInTuple<T extends MethodDescriptorTuple> = T[number];
 
 export class MethodDescriptorRegistry<T extends MethodDescriptorTuple> {
   public readonly descriptorMap: MethodDescriptorMapOf<T>;
-  private readonly rpcRequestSchema: RpcRequestSchemaOf<T>;
 
   constructor(private readonly tuple: T) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- Need to use the any type here since the object can't be initialized as the final desired type.
     this.descriptorMap = this.tuple.reduce((acc, cur) => {
       return {
         ...acc,
-        [cur.methodName]: cur,
+        [cur.method]: cur,
       };
     }, {}) as any;
-
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- Need to use the any type here since zod union type is too complex for internal inference
-    this.rpcRequestSchema = z.union(
-      tuple.map((descriptor) => descriptor.requestSchema) as any
-    );
   }
 
   public getDescriptorByName(
     methodName: string
   ): SomeDescriptorInTuple<T> | undefined {
-    return this.tuple.find(
-      (descriptor) => descriptor.methodName === methodName
-    );
-  }
-
-  public clone(): MethodDescriptorRegistry<T> {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- Need to use the any type here since .map() loses the tuple generic type inference.
-    const clonedTuple: T = this.tuple.map((descriptor) =>
-      descriptor.clone()
-    ) as any;
-
-    return new MethodDescriptorRegistry(clonedTuple);
+    return this.tuple.find((descriptor) => descriptor.method === methodName);
   }
 }
