86ff7e642f1fb49aa8d165ab82adac94f5826cf8
Kerem Kazan <kerem.kazan@gmail.com>
2024-01-15 10:19:41 -0500

    added blocktime to chains

--- packages/nexus/src/chain/chain.ts
+++ packages/nexus/src/chain/chain.ts
@@ -1,6 +1,19 @@
 export class Chain {
-  constructor(
-    public readonly name: string,
-    public readonly chainId: number
-  ) {}
+  public readonly name: string;
+  public readonly chainId: number;
+  public readonly blockTime: number;
+
+  constructor({
+    name,
+    chainId,
+    blockTime,
+  }: {
+    name: string;
+    chainId: number;
+    blockTime: number;
+  }) {
+    this.name = name;
+    this.chainId = chainId;
+    this.blockTime = blockTime;
+  }
 }
