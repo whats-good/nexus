411c596917f934723d00bd2ebe4c41d90f067b87
Kerem Kazan <kerem.kazan@gmail.com>
2024-01-18 05:34:26 -0500

    service providers handle building via keys

--- packages/nexus/src/service-provider/service-provider-registry.ts
+++ packages/nexus/src/service-provider/service-provider-registry.ts
@@ -1,18 +1,47 @@
+import type { Chain } from "../chain";
+import type { ChainSupport } from "./chain-support";
 import type { ServiceProvider } from "./service-provider";
+
+type ServiceProviderWithGuaranteedChainSupport = ServiceProvider & {
+  buildChainSupport: (chain: Chain, key?: string) => ChainSupport;
+};
 
 export class ServiceProviderRegistry {
   private readonly chainIdToProviders = new Map<number, ServiceProvider[]>();
 
-  public addProvider(provider: ServiceProvider) {
-    for (const chainSupport of provider.supportedChains.values()) {
-      const { chain } = chainSupport;
-      const chainProviders = this.chainIdToProviders.get(chain.chainId) ?? [];
+  public addServiceProvider(provider: ServiceProvider) {
+    const supportedChains = provider.getSupportedChains();
 
-      if (!this.chainIdToProviders.has(chain.chainId)) {
-        this.chainIdToProviders.set(chain.chainId, chainProviders);
+    for (const chain of supportedChains) {
+      const providers = this.chainIdToProviders.get(chain.chainId) ?? [];
+
+      if (providers.includes(provider)) {
+        throw new Error(
+          `Provider ${provider.name} is already registered for chain ${chain.name} (${chain.chainId})`
+        );
       }
 
-      chainProviders.push(provider);
+      providers.push(provider);
+
+      this.chainIdToProviders.set(chain.chainId, providers);
     }
+  }
+
+  public getProvidersForChain(
+    chainId: number
+  ): ServiceProviderWithGuaranteedChainSupport[] {
+    const providers = this.chainIdToProviders.get(chainId);
+
+    if (!providers) {
+      return [];
+    }
+
+    // we use this hack, even though a random service provider is not guaranteed to actually
+    // support an arbitrary chain, we know that by this point, this provider fully supports
+    // this chain, since it's coming from the chain support map. this hack helps us avoid
+    // having to do a type check on the return value of `buildChainSupport` in the caller
+    // of this function.
+
+    return providers as ServiceProviderWithGuaranteedChainSupport[];
   }
 }
--- packages/nexus/src/service-provider/service-provider.ts
+++ packages/nexus/src/service-provider/service-provider.ts
@@ -1,11 +1,70 @@
+import type { Chain } from "../chain";
 import type { ChainSupport } from "./chain-support";
+import {
+  KeyAppendedUrlChainSupport,
+  PureUrlChainSupport,
+} from "./chain-support";
+
+interface KeyAppendedUrlChainSupportInitArgs {
+  kind: "key-appended-url";
+  chain: Chain;
+  baseURL: string;
+}
+
+interface PureUrlChainSupportInitArgs {
+  kind: "pure-url";
+  chain: Chain;
+  url: string;
+}
+
+type ChainSupportInitArgs =
+  | KeyAppendedUrlChainSupportInitArgs
+  | PureUrlChainSupportInitArgs;
 
 export class ServiceProvider {
   constructor(public readonly name: string) {}
 
-  public readonly supportedChains = new Map<number, ChainSupport>();
+  public readonly chainSupportInitArgs = new Map<
+    number,
+    ChainSupportInitArgs
+  >();
 
-  public addChainSupport(support: ChainSupport) {
-    this.supportedChains.set(support.chain.chainId, support);
+  public addChainSupport(chainSupport: ChainSupportInitArgs) {
+    this.chainSupportInitArgs.set(chainSupport.chain.chainId, chainSupport);
+  }
+
+  public getSupportedChains(): Chain[] {
+    const chainSupportInitArgs = Array.from(this.chainSupportInitArgs.values());
+    const chains = chainSupportInitArgs.map(
+      (chainSupport) => chainSupport.chain
+    );
+
+    return chains;
+  }
+
+  public buildChainSupport(
+    chain: Chain,
+    key?: string
+  ): ChainSupport | undefined {
+    const chainSupport = this.chainSupportInitArgs.get(chain.chainId);
+
+    if (!chainSupport) {
+      return undefined;
+    }
+
+    switch (chainSupport.kind) {
+      case "key-appended-url": {
+        if (!key) {
+          throw new Error(
+            `Chain ${chain.name} (${chain.chainId}) requires a key to be specified`
+          );
+        }
+
+        return new KeyAppendedUrlChainSupport(chain, chainSupport.baseURL, key);
+      }
+
+      case "pure-url":
+        return new PureUrlChainSupport(chain, chainSupport.url);
+    }
   }
 }
