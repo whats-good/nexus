ee36796ef48315c546f81af48c579f75750435bc
Kerem Kazan <kerem.kazan@gmail.com>
2024-03-06 06:38:23 -0500

    standalone nodejs example wip

--- examples/nodejs-standalone-server/src/index.ts
+++ examples/nodejs-standalone-server/src/index.ts
@@ -1,9 +1,22 @@
-import { Nexus, CHAIN, NODE_PROVIDER } from "@whatsgood/nexus";
+import {
+  Nexus,
+  CHAIN,
+  NODE_PROVIDER,
+  queryParamKeyAuthMiddleware,
+} from "@whatsgood/nexus";
 import { createServer } from "node:http";
+
+if (process.env.ALCHEMY_KEY === undefined) {
+  throw new Error("ALCHEMY_KEY env var is required");
+}
+if (process.env.QUERY_PARAM_AUTH_KEY === undefined) {
+  throw new Error("QUERY_PARAM_AUTH_KEY env var is required");
+}
 
 const nexus = Nexus.create({
   nodeProviders: [NODE_PROVIDER.alchemy.build(process.env.ALCHEMY_KEY)],
   chains: [CHAIN.EthMainnet],
+  middlewares: [queryParamKeyAuthMiddleware(process.env.QUERY_PARAM_AUTH_KEY)],
 });
 
 createServer(nexus).listen(4005, () => {
