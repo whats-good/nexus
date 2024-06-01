fbdd97a370a3389c07dde84c919ff850c41b0ccb
Kerem Kazan <kerem.kazan@gmail.com>
2024-06-01 18:37:36 -0400

    added safe json stringify

--- packages/nexus/src/utils/index.ts
+++ packages/nexus/src/utils/index.ts
@@ -13,3 +13,15 @@ export const randomizeArray = <T>(arr: T[]): T[] => {
 
   return copy;
 };
+
+export function safeJsonStringify(
+  value: any,
+  replacer?: (number | string)[] | null,
+  space?: string | number
+): string {
+  try {
+    return JSON.stringify(value, replacer, space);
+  } catch (error) {
+    return `[Error: Could not stringify value]`;
+  }
+}
