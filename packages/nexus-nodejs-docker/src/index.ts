73bd9b73ddcf4d26c2f5de5c89444040eebe16d5
Kerem Kazan <kerem.kazan@gmail.com>
2024-07-21 17:28:34 -0400

    ws interface update

--- packages/nexus-nodejs-docker/src/index.ts
+++ packages/nexus-nodejs-docker/src/index.ts
@@ -3,10 +3,8 @@ import { createServer } from "node:http";
 
 const nexus = Nexus.create();
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises -- this promise is safe
 const server = createServer(nexus);
-const nwss = nexus.wsServer();
-server.on("upgrade", nwss.handleUpgrade.bind(nwss));
+nexus.ws(server);
 
 server.listen(nexus.port, () => {
   nexus.logger.info(`🚀 Server ready at http://localhost:${nexus.port}`);
