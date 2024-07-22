98302a21c9a39946d2fd09059214016378a3fcde
Kerem Kazan <kerem.kazan@gmail.com>
2024-07-22 05:43:06 -0400

    removing custom event handler

--- packages/nexus/src/events/event-bus.ts
+++ packages/nexus/src/events/event-bus.ts
@@ -1,26 +1,21 @@
-import type { Logger } from "pino";
+import { EventEmitter } from "eventemitter3";
 import type { NexusRpcContext } from "@src/dependency-injection";
-import type { NexusEvent } from "./nexus-event";
+import type { RpcErrorResponse, RpcSuccessResponse } from "@src/rpc-response";
 
-// TODO: settle on a naming convention: NexusEventBus vs EventBus etc
-
-export class EventBus<TPlatformContext = unknown> {
+export class EventBus<TPlatformContext = unknown> extends EventEmitter<{
+  rpcResponseSuccess: (
+    response: RpcSuccessResponse,
+    ctx: NexusRpcContext<TPlatformContext>
+  ) => void;
+  rpcResponseError: (
+    response: RpcErrorResponse,
+    ctx: NexusRpcContext<TPlatformContext>
+  ) => void;
+}> {
   private readonly ctx: NexusRpcContext<TPlatformContext>;
-  private readonly eventQueue: NexusEvent[] = [];
-  private readonly logger: Logger;
-  // TODO: perhaps, ctx based loggers should have a reference to the request Id, or some other unique identifier
-  // TODO: in fact, the logger should be spawned from the ctx, not ctx.parent.
-  // TODO: but also make sure that static container logs can be tied back to the request too. maybe this is a reason not to spawn loggers from the ctx.
 
   constructor(params: { ctx: NexusRpcContext<TPlatformContext> }) {
+    super();
     this.ctx = params.ctx;
-    this.logger = this.ctx.container.logger.child({
-      name: this.constructor.name,
-    });
-  }
-
-  // TODO: give ways to enqueue ordered and unordered events
-  public dispatch(event: NexusEvent) {
-    this.eventQueue.push(event);
   }
 }
